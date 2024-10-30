14a2b610ca8604f1058577b3bd5caa99b4170015
Gleb <w>
2024-10-30 17:27:25 +0300

    Initial Commit

---
+++ TestBuild_!Tests/TestBuild__Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TestBuild__Tests.swift
+//  TestBuild_!Tests
+//
+//  Created by Глеб Соколов on 30.10.2024.
+//
+
+import Testing
+@testable import TestBuild__
+
+struct TestBuild__Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
